f58f73f19bf21e5f0b1c88452bb45de847857117
Elvis Lewis <elvislewisjr@elviss-mbp-2.attlocal.net>
2022-08-12 13:02:15 -0400

    Let Const Exercise

---
+++ LetConst.js
@@ -0,0 +1,13 @@
+//Global Constants
+let PI = 3.14
+PI = 42
+
+//Quiz
+
+//Var is function blocked where let is code blocked.  Var can redeclare and reassign but Let only lets you reassign
+
+//Const you cannot redeclare nor reassign; Var you can do both.
+
+//let you can reassign, const you cannot
+
+//Hoisting refers to Var ability to move up in its block to be recognized first.
